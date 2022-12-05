be6b7b8d1956c7a9e28023d1fc0f81f34d2d3adf
LuciaRyan <lcryan06@gmail.com>
2022-12-05 12:30:26 +0100

    4. called on API

--- src/App.js
+++ src/App.js
@@ -4,43 +4,57 @@ import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
 import axios from 'axios';
-const apiKey = '5f8a63406e1b065089f3f5e6e984050f';
 
+const apiKey = '7613e22655f9590922054d0746226b6e';
 
 
 function App() {
-  return (
-    <>
-      <div className="weather-container">
 
-        {/*HEADER -------------------- */}
-        <div className="weather-header">
-          <SearchBar/>
+    async function fetchData() {
+        try {
+            const result = await
+               axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
+            console.log(result.data);
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
-          <span className="location-details">
+
+    return (
+        <>
+            <div className="weather-container">
+
+                {/*HEADER -------------------- */}
+                <div className="weather-header">
+                    <SearchBar/>
+
+                    <span className="location-details">
             <h2>Bewolkt</h2>
             <h3> </h3>
             <h1>14 &deg;</h1>
 
-            <button type="button">
+            <button
+                type="button"
+                onClick={fetchData}>
               Haal data op!
             </button>
           </span>
-        </div>
+                </div>
 
-        {/*CONTENT ------------------ */}
-        <div className="weather-content">
-          <TabBarMenu/>
+                {/*CONTENT ------------------ */}
+                <div className="weather-content">
+                    <TabBarMenu/>
 
-          <div className="tab-wrapper">
-            Alle inhoud van de tabbladen komt hier!
-          </div>
-        </div>
+                    <div className="tab-wrapper">
+                        Alle inhoud van de tabbladen komt hier!
+                    </div>
+                </div>
 
-        <MetricSlider/>
-      </div>
-    </>
-  );
+                <MetricSlider/>
+            </div>
+        </>
+    );
 }
 
 export default App;
